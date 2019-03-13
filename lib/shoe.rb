d2c63158eac08243e0f04703dbc934294b9e7312
Oliver <oliver.grant@gmail.com>
2019-03-13 14:36:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << @brand if BRANDS.find { |a_brand| @brand } == nil
+    if BRANDS.find { |a_brand| @brand } == nil
+      BRANDS << @brand
+    end
   end
 
   def cobble
